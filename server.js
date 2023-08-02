7a5662fc6ae948ce3c3d6a73adac8bbcf64332f2
Tom_Court <tomcourt94@icloud.com>
2023-08-02 16:02:34 +0100

    changed port to 3333

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 // server.js
-const express = require('express');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3333;
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Contact form route
-app.post('/api/contact', (req, res) => {
+app.post("/api/contact", (req, res) => {
   const { name, email, message } = req.body;
 
   // Create a Nodemailer transporter
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "Gmail",
     auth: {
-      user: 'tomcourt.softwareengineer@gmail.com',
-      pass: 'K983mwhatsername!', // Replace with your actual email password
+      user: "tomcourt.softwareengineer@gmail.com",
+      pass: "K983mwhatsername!",
     },
   });
 
   // Configure the email message
   const mailOptions = {
     from: email,
-    to: 'tomcourt.softwareengineer@gmail.com',
-    subject: 'Portfolio Contact Form Submission',
+    to: "tomcourt.softwareengineer@gmail.com",
+    subject: "Contact Form Submission",
     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
   };
 
@@ -34,10 +34,10 @@ app.post('/api/contact', (req, res) => {
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error);
-      res.status(500).json({ error: 'Failed to send the email' });
+      res.status(500).json({ error: "Failed to send the email" });
     } else {
-      console.log('Email sent: ' + info.response);
-      res.json({ message: 'Email sent successfully' });
+      console.log("Email sent: " + info.response);
+      res.json({ message: "Email sent successfully" });
     }
   });
 });
